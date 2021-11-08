cb76279ae62a7097ea9393e3a489d36936e01a64
localDev1 <fproblero85@gmail.com>
2021-11-08 10:53:43 -0800

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,22 @@
+
+/*FUNCTION ASSIGNMENT*/
+function displayDate() 
+{
+    //Creating two variables 
+    var x = "the time is: ", y="  PERFECT FOR RUNNIG!!!"
+
+    //USing the getElementById to call this function in the HTML code using id demo
+    document.getElementById("demo").innerHTML = x + Date()+ y;
+}
+
+
+/*    += OPERATOR ASSIGNMENT */
+
+function operator() 
+{
+    //Creating a variable using the += operator
+    var x = "My name is: ";
+    x +="  Fernando Perez Roblero"
+    //USing the getElementById to call this function in the HTML code using id name
+    document.getElementById("name").innerHTML = x;
+}
