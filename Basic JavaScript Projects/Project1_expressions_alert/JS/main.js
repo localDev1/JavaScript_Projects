93e7653413f5ea4fb85b33b244cf61b25fb646aa
localDev1 <fproblero85@gmail.com>
2021-11-08 10:16:08 -0800

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+//windows alert
+window.alert("Hello, \"This is a string\"");
+
+var A= "\"This is a string\"";
+var B = "    string to concatenate" +"<br>";
+
+/*multiple variables*/
+var x= "hello", y=" world ", z="how are you", dad ="Fernando", mom="Tania",daugther="Maggie";
+
+/* colors assignment*/
+var A =A.fontcolor("red"), B=B.fontcolor("blue"), dad=dad.fontcolor("blue");
+
+//concatening two variables
+document.write(A + B);
+
+/*concatenating*/
+document.write("Hello World!!" + "   How are you???? concatenated"+"<br>");
+
+document.write("multiple variables : " + dad+"<br>");
+
+/*expression assignment*/
+var c=5*5;
+document.write(c);
